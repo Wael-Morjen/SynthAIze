1c5d21d6c89359decd8638481acfa4ea9e116726
wael-morjen <waelmorjen@gmail.com>
2023-08-05 23:58:55 +0100

    clerk auth integration

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
     return (
      <div>
         Dashboard Page (Protected)
+        <UserButton afterSignOutUrl="/"/>
      </div>
     )
   }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
     return (
         <div>
             Landing Page (Unprotected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+            </div>
+            <div>
+                <Link href="/sign-up">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
